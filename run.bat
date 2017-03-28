4310eb5f65a8d708068610fea68b9ec287cc1016
Dylan Cross <dylancross@me.com>
2017-03-28 15:35:44 +1300

    Batch file to clean/make and run the server file on port 1155

---
+++ run.bat
@@ -0,0 +1,24 @@
+cls
+@echo off
+
+@echo Clean folder before making?
+@echo Y/N?
+
+set /p clean=
+@echo %clean%
+
+if %clean% == y (
+  @echo del *.o
+  del *.o
+  @echo del *.exe
+  del *.exe
+) else if %clean% == Y (
+  @echo del *.o
+  del *.o
+  @echo del *.exe
+  del *.exe
+)
+@echo make
+make
+@echo server.exe 1155
+server.exe 1155
